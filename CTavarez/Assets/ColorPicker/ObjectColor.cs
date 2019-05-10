6bc58aa109bee52d5883069ee49567cb614b7772
ctavarez907 <50502314+ctavarez907@users.noreply.github.com>
2019-05-10 18:15:39 -0400

    Add files via upload

---
+++ CTavarez/Assets/ColorPicker/ObjectColor.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectColor : MonoBehaviour
+{
+
+
+    void OnSetColor(Color color)
+    {
+        Material mt = GetComponent<Renderer>().sharedMaterial;
+        mt.color = color;
+        GetComponent<Renderer>().sharedMaterial = mt;
+    }
+
+    void OnGetColor(ColorPicker picker)
+    {
+        picker.NotifyColor(GetComponent<Renderer>().sharedMaterial.color);
+    }
+
+    void OnApplicationQuit()
+    {
+        GetComponent<Renderer>().sharedMaterial.color = Color.white;
+
+    }
+}
