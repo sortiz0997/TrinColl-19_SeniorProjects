a2f60eea6a9b77964194c2367d206244c9ef8d40
ctavarez907 <50502314+ctavarez907@users.noreply.github.com>
2019-05-10 18:18:53 -0400

    Add files via upload

---
+++ CTavarez/Assets/Scripts/MainMenuLoad.cs
@@ -0,0 +1,16 @@
+﻿/* Clear A. Tavarez, 2019
+ *
+ * This script loads the main menu at startup and whenever a linked button is pressed */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuLoad : MonoBehaviour {
+
+	void Start () {
+        SceneManager.LoadScene("Main Menu");
+	}
+	
+}
