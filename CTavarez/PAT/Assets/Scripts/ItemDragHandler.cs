c5d26e52265a5f32add7606c61c2f87677c9db75
ctavarez907 <50502314+ctavarez907@users.noreply.github.com>
2019-05-10 18:36:48 -0400

    Project Upload

---
+++ CTavarez/PAT/Assets/Scripts/ItemDragHandler.cs
@@ -0,0 +1,18 @@
+﻿/* Clear A. Tavarez, 2019
+ * 
+ * This script drags an item to the mouse position and leaves it where the user left it */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ItemDragHandler : MonoBehaviour, IDragHandler
+{
+
+    //moves linked object to the spot the user drags it to
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = Input.mousePosition;
+    }
+}
---
+++ CTavarez/PAT/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,32 @@
+﻿/* Clear A. Tavarez, 2019
+ * Made with help from Brackeys: https://www.youtube.com/watch?v=zc8ac_qUXQY 
+ * 
+ * This script loads the appropriate scene when the linked button is pressed */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Tut1");
+
+    }
+
+    public void PlayGame2()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+
+    }
+
+    public void PlayGame3()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 3);
+
+    }
+
+}
